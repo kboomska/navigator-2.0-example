adef3fb0a17676fab48830837f184a3ac7ab629b
Aleksey Kuzmin <kboomska@gmail.com>
2023-10-29 16:12:07 +0300

    Pages: Custom transition animation

--- lib/main.dart
+++ lib/main.dart
@@ -100,12 +100,30 @@ class BookDetailsPage extends Page {
     required this.book,
   }) : super(key: ValueKey(book));
 
+  // @override
+  // Route createRoute(BuildContext context) {
+  //   return MaterialPageRoute(
+  //     settings: this,
+  //     builder: (BuildContext context) {
+  //       return BookDetailsScreen(book: book);
+  //     },
+  //   );
+  // }
+
   @override
   Route createRoute(BuildContext context) {
-    return MaterialPageRoute(
+    return PageRouteBuilder(
       settings: this,
-      builder: (BuildContext context) {
-        return BookDetailsScreen(book: book);
+      pageBuilder: (context, animation, animation2) {
+        final tween = Tween(begin: const Offset(0.0, 1.0), end: Offset.zero);
+        final curveTween = CurveTween(curve: Curves.easeInOut);
+        return SlideTransition(
+          position: animation.drive(curveTween).drive(tween),
+          child: BookDetailsScreen(
+            key: ValueKey(book),
+            book: book,
+          ),
+        );
       },
     );
   }
