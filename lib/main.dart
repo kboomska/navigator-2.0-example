142c1fccf566aa07e35bd25a502c76f1e7b18d99
Aleksey Kuzmin <kboomska@gmail.com>
2023-10-29 17:51:01 +0300

    Router: RouteInformationParser

--- lib/main.dart
+++ lib/main.dart
@@ -254,6 +254,47 @@ class BookRouterDelegate extends RouterDelegate<BookRoutePath>
   }
 }
 
+class BookRouteInformationParser extends RouteInformationParser<BookRoutePath> {
+  @override
+  Future<BookRoutePath> parseRouteInformation(
+      RouteInformation routeInformation) async {
+    final uri = Uri.parse(routeInformation.location!);
+    // Handle '/'
+    if (uri.pathSegments.isEmpty) {
+      return BookRoutePath.home();
+    }
+
+    // Handle '/book/:id'
+    if (uri.pathSegments.length == 2) {
+      if (uri.pathSegments[0] != 'book') return BookRoutePath.unknown();
+
+      final remaining = uri.pathSegments[1];
+      final id = int.tryParse(remaining);
+
+      if (id == null) return BookRoutePath.unknown();
+
+      return BookRoutePath.details(id);
+    }
+
+    // Handle unknown routes
+    return BookRoutePath.unknown();
+  }
+
+  @override
+  RouteInformation? restoreRouteInformation(BookRoutePath configuration) {
+    if (configuration.isUnknown) {
+      return const RouteInformation(location: '/404');
+    }
+    if (configuration.isHomePage) {
+      return const RouteInformation(location: '/');
+    }
+    if (configuration.isDetailsPage) {
+      return RouteInformation(location: '/book/${configuration.id}');
+    }
+    return null;
+  }
+}
+
 class UnknownScreen extends StatelessWidget {
   const UnknownScreen({super.key});
 
