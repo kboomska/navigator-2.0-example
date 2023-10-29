d73dea07504a2c44678b0513535c73bf0f872954
Aleksey Kuzmin <kboomska@gmail.com>
2023-10-29 15:01:36 +0300

    Navigator 2.0 exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Nav2App());
+  runApp(const BooksApp());
 }
 
-class Nav2App extends StatelessWidget {
-  const Nav2App({super.key});
+class Book {
+  final String title;
+  final String author;
+
+  Book(this.title, this.author);
+}
+
+class BooksApp extends StatefulWidget {
+  const BooksApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _BooksAppState();
+}
+
+class _BooksAppState extends State<BooksApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      onGenerateRoute: (settings) {
-        // Handle '/'
-        if (settings.name == '/') {
-          return MaterialPageRoute(builder: (context) => const HomeScreen());
-        }
-
-        // Handle '/details/:id'
-        final uri = Uri.parse(settings.name ?? '');
-        if (uri.pathSegments.length == 2 &&
-            uri.pathSegments.first == 'details') {
-          final id = uri.pathSegments[1];
-          return MaterialPageRoute(builder: (context) => DetailScreen(id: id));
-        }
-
-        return MaterialPageRoute(builder: (context) => const UnknownScreen());
-      },
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: TextButton(
-          child: const Text('View Details'),
-          onPressed: () {
-            Navigator.pushNamed(
-              context,
-              '/details/1',
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  final String id;
-
-  const DetailScreen({
-    super.key,
-    required this.id,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Viewing details for item $id'),
-            TextButton(
-              child: const Text('Pop!'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class UnknownScreen extends StatelessWidget {
-  const UnknownScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('404!'),
+      title: 'Books App',
+      home: Navigator(
+        pages: const [
+          MaterialPage(
+            key: ValueKey('BooksListPage'),
+            child: Scaffold(),
+          )
+        ],
+        onPopPage: (route, result) => route.didPop(result),
       ),
     );
   }
